f5ed5868d51b7a82cb5ae8ad79b9bfcc13e4b308
Muslehud77 <fardinmohit@gmail.com>
2024-01-23 04:05:04 +0800

    created routes

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,34 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from './Root/Root';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Register from './Pages/Register/Register';
+import Dashboard from './Pages/Dashboard/Dashboard';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root></Root>
+    element: <Root></Root>,
+    children: [
+      {
+      path: '/',
+      element: <Home/>
+    },
+    {
+      path: '/login',
+      element: <Login/>
+    },
+    {
+      path: '/register',
+      element: <Register/>
+    },
+    {
+      path: '/dashboard',
+      element: <Dashboard/>
+    }
+    
+  ]
   },
 ]);
 
